1768e6c38d5d10739a9ae99b140c36d1ee7a8481
MCDesign <contacto@mauriciocasado.com>
2025-07-09 10:57:34 -0600

    ajuste en routing i18n

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,11 +1,13 @@
-
 import { defineRouting } from 'next-intl/routing';
  
+export const locales = ['en', 'es'] as const;
+
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'es'],
+  locales,
  
   // Used when no locale matches
   defaultLocale: 'en',
-});
+
+})
 
