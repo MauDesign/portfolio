e08e8ce2ea42873ecbaf449374918cc5541d7336
MCDesign <contacto@mauriciocasado.com>
2025-07-21 17:12:12 -0600

    Ajuste imagenes  y metadata e imagen

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,23 +1,58 @@
-import {Locale, useTranslations} from 'next-intl';
-import {use} from 'react';
+
 import Projects from "@/ui/projects/projects";
 import Skills from "@/ui/skills/skills";
 import Contact from "@/ui/contact/contact";
 import Header from "@/ui/header/header";
-import { setRequestLocale } from 'next-intl/server';
+import { getTranslations, setRequestLocale } from 'next-intl/server';
+import type { Metadata } from 'next';
 
 type Props = {
- params:Promise<{ locale: Locale}>;
+ params: { locale: string };
 };
 
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  // Accede a 'locale' dentro del cuerpo de la función
+  const { locale } = props.params;
 
-export default function Home ({params}: Props) {
+  // Leer traducciones desde el servidor
+  const t = await getTranslations({ locale, namespace: 'HeaderHero' });
+  const autor = t('autor');
+  const description = t('frase');
 
-  const {locale} = use(params);
+  return {
+    title: `${autor} | UX/UI Designer Portfolio`,
+    description: description,
+    openGraph: {
+      title: `${autor} | UX/UI Designer Portfolio`,
+      description: description,
+      url: `https://www.mauriciocasado.com/${locale}`, // ¡Asegúrate de usar tu dominio real!
+      siteName: `${autor} Portfolio`,
+      images: [
+        {
+          url: '/img/MWU_MC.jpg', // Coloca una imagen llamada 'og-image.png' en tu carpeta /public (tamaño recomendado: 1200x630px)
+          width: 1200,
+          height: 630,
+          alt: `Portafolio de ${autor}`,
+        },
+      ],
+      locale: locale,
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${autor} | UX/UI Designer Portfolio`,
+      description: description,
+      images: ['/img/MWU_MC.jpg'], // La misma imagen para Twitter
+    },
+  };
+}
+
+
+export default async function Home ({params}: Props) {
+
+  const { locale } = params;
   setRequestLocale(locale);
-  
-  const t= useTranslations('Home');
-
+  const t = await getTranslations('Home');
  
 return (
    
@@ -25,13 +60,13 @@ return (
     <div className="w-full">
 
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Header/>
+        <Header />
         <div className='max-w-3/4 m-auto'>
-          <Projects />
+          <Projects  />
         </div>
         <div className='w-full bg-gradient-to-r from-teal-200 to-teal-500'>
           <div className='max-w-3/4 m-auto'>
-            <Skills />
+            <Skills  />
           </div>
         </div>
         <Contact locale={locale}/>
