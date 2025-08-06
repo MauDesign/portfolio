5cb1442495471f0f6c5f78b85afba8469a27564d
MCDesign <contacto@mauriciocasado.com>
2025-08-06 10:35:56 -0600

    ajustes  i18n  y pnpm

--- i18n.ts
+++ i18n.ts
@@ -3,12 +3,12 @@ import { getRequestConfig } from "next-intl/server";
 // Importa 'locales' desde tu archivo centralizado de routing
 import { locales } from "./src/i18n/routing"; 
 
-export default getRequestConfig(async({locale})=>{
+export default getRequestConfig(async({ locale })=>{
     // Valida que el locale entrante es válido.
     if(!locales.includes(locale as typeof locales[number])) notFound(); 
             
     return{
-        locale,
+        locale: locale,
         messages: (await import(`./messages/${locale}.json`)).default,
     };
 });
