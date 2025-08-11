6ae1817815ddc605075b627ad90247323ddf460b
MCDesign <contacto@mauriciocasado.com>
2025-08-11 14:25:05 -0600

    fix dploy messages

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -58,7 +58,7 @@ type Props = {
 export default async function LocaleLayout({children, params}: Props) {
   const {locale} = await params;
   setRequestLocale(locale);
-  const messages = await getMessages();
+ 
 
   return (
     <html lang={locale}>
